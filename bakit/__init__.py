b691daf53778dea53a78f3f1777d461ecb37a69e
nawafalqari <nawafalqari13@gmail.com>
2022-11-08 18:12:38 +0300

    Added copy (built-in module) alternative

---
+++ bakit/__init__.py
@@ -0,0 +1,13 @@
+'''
+# BAKit
+Built-in Alternative Kit
+
+'''
+
+from .Events import Event, Events
+from .Queue import Queue
+from .TasksQueue import TasksQueue
+from .Copy import copy
+
+__version__ = "1.0.1"
+__all__ = ["Event", "Events", "Queue", "TasksQueue"]
--- tests.py
+++ tests.py
@@ -1,13 +1,2 @@
-from time import sleep
-from bakit.TasksQueue import TasksQueue
+from bakit.Copy import copy
 
-tq = TasksQueue(3)
-
-def task(s=1):
-   print(f"sleeping for {s}")
-   sleep(s)
-   print(f"finished {s}")
-
-tq.execute(task)
-tq.execute(task, 2)
-tq.execute(task, 3)
